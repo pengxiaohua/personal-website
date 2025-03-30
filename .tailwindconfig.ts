864481ac67eca7416f315a9d6a26925408c8e348
Xiaohua Peng <609370075@qq.com>
2025-03-30 23:52:55 +0800

    feat: 修改配置

---
+++ .tailwindconfig.ts
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        foreground: 'var(--foreground-color, #000)', // 添加前景色变量
+      },
+    },
+  },
+  plugins: [],
+};
