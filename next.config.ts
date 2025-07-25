da3ed1d37897187e7f829f3e4a060431b4de2747
Xiaohua Peng <609370075@qq.com>
2025-07-25 22:55:55 +0800

    feat: 修改配置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true, // 禁用图片优化，直接使用原图
+  }
 };
 
 export default nextConfig;
